e86f5f02c7c7bee797f86997ca0e1c7460378eee
EliasBasil <elijah89@mail.ru>
2024-10-02 15:23:36 +0300

    Homework 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        int[] monthlyLedger = {50, 100, 200, 500, 1000};
+        int ledgerSum = 0;
+        for (int i : monthlyLedger) {
+            ledgerSum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + ledgerSum + " рублей");
+
+        // Задача 2
+        int[] weeklyLedger = {234, 89, 9, 1861, 480};
+        int minSum = 65535;
+        int maxSum = -1;
+        for (int j : weeklyLedger) {
+            if (j > maxSum) {
+                maxSum = j;
+            }
+            if (j < minSum) {
+                minSum = j;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minSum + " рублей. " +
+                "Максимальная сумма трат за неделю составила " + maxSum + " рублей");
+
+        // Задача 3
+        int[] weeklyLedger2 = {384, 8, 1765, 861, 80};
+        int ledgerSum2 = 0;
+        for (int i : weeklyLedger2) {
+            ledgerSum2 += i;
+        }
+        double average = (double) ledgerSum2 / weeklyLedger2.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+
+        // Задача 4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char currentChar = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = currentChar;
+        }
+        for (char c : reverseFullName) {
+            System.out.print(c);
+        }
     }
 }
